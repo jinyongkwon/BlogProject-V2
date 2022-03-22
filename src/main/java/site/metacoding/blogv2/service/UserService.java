46261854b4b951513269d5e9f32b0fe76a57de6e
jinyongkwon <wlsdyd1178@naver.com>
2022-03-22 15:01:12 +0900

    7.로그인(자동저장제외)구현완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.domain.user.UserRepository;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
+import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 
 @RequiredArgsConstructor
 @Service // 컴포넌트 스캔시에 IOC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임.
@@ -17,5 +19,10 @@ public class UserService {
     public void 회원가입(JoinDto joinDto) {
         // Save하면 DB에 INSERT한 결과를 다시 return 해준다.
         userRepository.save(joinDto.toEntity());
+    }
+
+    public User 로그인(LoginDto loginDto) {
+        User userEntity = userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
+        return userEntity;
     }
 }
