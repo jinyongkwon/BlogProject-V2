65280e9b5d8d4e7e64ad511dee3c904839073a95
jinyongkwon <wlsdyd1178@naver.com>
2022-03-31 15:49:07 +0900

    15.게시글 목록 페이징 및, 페이지이동 버튼 완료

--- src/main/java/site/metacoding/blogv2/service/PostService.java
+++ src/main/java/site/metacoding/blogv2/service/PostService.java
@@ -1,7 +1,14 @@
 package site.metacoding.blogv2.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +22,12 @@ import site.metacoding.blogv2.domain.post.PostRepository;
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public Page<Post> 글목록보기(Integer page) {
+        PageRequest pq = PageRequest.of(page, 3, Sort.by(Direction.DESC, "id"));
+        Page<Post> posts = postRepository.findAll(pq);
+        return posts;
+    }
 
     // 그냥 안함
     @Transactional
--- src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
@@ -1,9 +1,15 @@
 package site.metacoding.blogv2.web.api;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,12 +27,17 @@ public class PostApiController {
 
     @PostMapping("/s/post")
     public ResponseDto<?> write(@RequestBody WriteDto writeDto) {
-
         User principal = (User) session.getAttribute("principal");
         Post post = writeDto.toEntity(principal);
         // 원래는 그냥 Dto를 바로 넘겼는데, 지금 Dto를 넘기면 session 값 두개 넘겨야 해서 하나로 합쳐서 넘김.
         postService.글쓰기(post);
-
         return new ResponseDto<>(1, "성공", null);
+    }
+
+    @GetMapping("/api/post")
+    public ResponseDto<?> list(Integer page) {
+        Page<Post> posts = postService.글목록보기(page);
+        // 응답의 DTO를 만들어서 <- posts를 옮김. (라이브러리 있음)
+        return new ResponseDto<>(1, "성공", posts);
     }
 }
